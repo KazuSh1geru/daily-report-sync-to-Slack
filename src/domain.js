c548b22788994e59a31705e10da3887c1c1ad6ac
髙草木 和史 <kazushi.takakusagi@gmail.com>
2023-07-10 18:22:49 +0900

    domain分離

---
+++ src/domain.js
@@ -0,0 +1,45 @@
+function makeSlackWorkspacePropertyKeysList() {
+	// 本番用
+	let slack_workspace_property_keys_list = [
+		{
+			webhook_url: 'webhook_url_pd',
+			oauth: 'oauth_pd',
+		},
+		{
+			webhook_url: 'webhook_url_reskill',
+			oauth: 'oauth_reskill',
+		},
+	];
+	// デバッグ用
+	// let slack_workspace_property_keys_list = [
+	//     {
+	//         webhook_url: 'webhook_url_test',
+	//         oauth: 'oauth_reskill',
+	//     },
+	//     {
+	//         webhook_url: 'webhook_url_pd_test',
+	//         oauth: 'oauth_pd',
+	//     },
+	// ];
+	return slack_workspace_property_keys_list;
+}
+
+// Webhook URLの対応関係を保存したテーブル(Spreadsheet)を取得する
+function makeWebhookUrlTableConfig() {
+	let config = {
+		sheet_id: '1Mda9n-b2JmfQW4MbyhAS_LoBTn-d3C-30VOshrrLD9k',
+		sheet_name: 'webhook_urls',
+	};
+	return config;
+}
+
+// 日報提出管理シートへの書き込みを行う
+function makeSendingLogTableConfig() {
+	let config = {
+		sheet_id: '15SPAug5rR6cXhblNrUl_VbycjbDMsSyhbk1mdaheupw',
+		sheet_name: '元データ_投稿',
+		channel_id: '<#C0547T4KK61>',
+		group_name: '室付',
+	};
+	return config;
+}
