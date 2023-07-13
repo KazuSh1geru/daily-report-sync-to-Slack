296eb103decdd0fd5eacbb37f870a6609f3455c1
髙草木 和史 <kazushi.takakusagi@gmail.com>
2023-07-13 14:35:59 +0900

    debugモード実装

--- src/slack.js
+++ src/slack.js
@@ -1,14 +1,3 @@
-function autoSlack(e) {
-	let slack_workspace_property_keys_list = makeSlackWorkspacePropertyKeysList();
-	for (var i = 0; i < slack_workspace_property_keys_list.length; i++) {
-		var property_keys = slack_workspace_property_keys_list[i];
-		var webhook_url_key = property_keys.webhook_url;
-		var token_key = property_keys.oauth;
-		// Slackへ送信する
-		sendSlack(e, token_key, webhook_url_key);
-	}
-}
-
 function sendSlack(e, token_key, webhook_url_key) {
 	// Slackの本文
 	let body = createBodyFromResponse(e, token_key);
